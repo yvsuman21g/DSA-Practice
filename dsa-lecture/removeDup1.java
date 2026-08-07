9a13877949509f2f830e928251919623db3bf7b1
sumanyadav <yvsuman21g@gmail.com>
2026-08-07 17:07:52 +0530

    DSA lecture

---
+++ dsa-lecture/removeDup1.java
@@ -0,0 +1,43 @@
+/*
+Input: arr[] = [2, 3, 1, 2, 3]
+Output: [2, 3] 
+Explanation: 2 and 3 occur twice in the given array.
+
+Input: arr[] = [3, 1, 2] 
+Output: [] 
+Explanation: There is no repeating element in the array, so the output is empty.
+
+*/
+import java.util.Scanner;
+public class removeDup1{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        // Input size of array
+        System.out.println("Enter the size of the array");
+        int n = sc.nextInt();
+
+//  enter the arrays elements
+System.out.println("Enter the array elements");
+        int[] arr = new int[n];
+        
+        for(int i=0; i< n; i++){
+           arr[i] = sc.nextInt(); 
+        }
+
+        int result = remoDupli(arr, n);
+        System.out.println("new array is: " + result);
+    }
+
+    public static int remoDupli(int[] arr, int n){
+        int x = 0;
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] != arr[x]){
+                x = x + 1;
+                arr[x] = arr[i];
+                return arr[x];
+            }
+        }
+        return x;
+    }
+}
